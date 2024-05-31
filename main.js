aded88f1b486b29265f04fc1548c5359af8aef5f
ranieripaiva <63205834+ranieripaiva@users.noreply.github.com>
2024-05-31 15:22:02 -0300

    Finalização

--- main.js
+++ main.js
@@ -1,28 +1,223 @@
 import { generateReturnsArray } from "./src/investmentGoals"
+import { Chart } from 'chart.js/auto';
+import { createTable } from './src/table';
 
-const form = document.getElementById('investiment-form')
+const finalMoneyChart = document.getElementById('final-money-distribution');
+const progressionChart = document.getElementById('progression');
+const form = document.getElementById('investment-form');
+const clearFormButton = document.getElementById('clear-form');
 //const calculateButton = document.getElementById('calculate-results')
+let doughnutChartReference = {};
+let progressionChartReference = {};
 
-function renderProgression(evt){
-  evt.preventDefault()
-  const startingAmount = Number(document.getElementById('starting-amount').value)
-  const additionalContribution = Number(document.getElementById('additional-contribution').value)
-  const timeAmount = Number( document.getElementById('time-amount').value)
-  const timeAmountPeriod = document.getElementById('time-amount-period').value
-  const returnRate = Number(document.getElementById('return-rate').value)
-  const returnRatePeriod = document.getElementById('evaluation-period').value
-  const taxRate = Number(document.getElementById('tax-rate').value)
+const columnsArray = [
+  { columnLabel: 'Mês', accessor: 'month' },
+  {
+    columnLabel: 'Total Investido',
+    accessor: 'investedAmount',
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
+  },
+  {
+    columnLabel: 'Rendimento Mensal',
+    accessor: 'interestReturns',
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
+  },
+  {
+    columnLabel: 'Rendimento Total',
+    accessor: 'totalInterestReturns',
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
+  },
+  {
+    columnLabel: 'Quantia Total',
+    accessor: 'totalAmount',
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
+  },
+];
+
+function formatCurrencyToTable(value) {
+  return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
+function formatCurrencyToGraph(value) {
+  return value.toFixed(2);
+}
+
+function renderProgression(evt) {
+  evt.preventDefault();
+  if (document.querySelector('.error')) {
+    return;
+  }
+  resetCharts();
+  // const startingAmount = Number(form['startingAmount'].value);
+  const startingAmount = Number(
+    document.getElementById('starting-amount').value.replace(',', '.')
+  );
+  const additionalContribution = Number(
+    document.getElementById('additional-contribution').value.replace(',', '.')
+  );
+  const timeAmount = Number(document.getElementById('time-amount').value);
+  const timeAmountPeriod = document.getElementById('time-amount-period').value;
+  const returnRate = Number(
+    document.getElementById('return-rate').value.replace(',', '.')
+  );
+  const returnRatePeriod = document.getElementById('evaluation-period').value;
+  const taxRate = Number(
+    document.getElementById('tax-rate').value.replace(',', '.')
+  );
 
   const returnsArray = generateReturnsArray(
-    startingAmount, 
-    timeAmount, 
-    timeAmountPeriod, 
-    additionalContribution, 
-    returnRate, 
+    startingAmount,
+    timeAmount,
+    timeAmountPeriod,
+    additionalContribution,
+    returnRate,
     returnRatePeriod
-  )
-  console.log(returnsArray)
+  );
 
+  const finalInvestmentObject = returnsArray[returnsArray.length - 1];
+
+  doughnutChartReference = new Chart(finalMoneyChart, {
+    type: 'doughnut',
+    data: {
+      labels: ['Total investido', 'Rendimento', 'Imposto'],
+      datasets: [
+        {
+          data: [
+            formatCurrencyToGraph(finalInvestmentObject.investedAmount),
+            formatCurrencyToGraph(
+              finalInvestmentObject.totalInterestReturns * (1 - taxRate / 100)
+            ),
+            formatCurrencyToGraph(
+              finalInvestmentObject.totalInterestReturns * (taxRate / 100)
+            ),
+          ],
+          backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(54, 162, 235)',
+            'rgb(255, 205, 86)',
+          ],
+          hoverOffset: 4,
+        },
+      ],
+    },
+  });
+
+progressionChartReference = new Chart(progressionChart, {
+  type: 'bar',
+  data: {
+    labels: returnsArray.map((investmentObject) => investmentObject.month),
+    datasets: [
+      {
+        label: 'Total Investido',
+        data: returnsArray.map((investmentObject) =>
+          formatCurrencyToGraph(investmentObject.investedAmount)
+        ),
+        backgroundColor: 'rgb(255, 99, 132)',
+      },
+      {
+        label: 'Retorno do Investimento',
+        data: returnsArray.map((investmentObject) =>
+          formatCurrencyToGraph(investmentObject.interestReturns)
+        ),
+        backgroundColor: 'rgb(54, 162, 235)',
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    scales: {
+      x: {
+        stacked: true,
+      },
+      y: {
+        stacked: true,
+      },
+    },
+  },
+});
+
+createTable(columnsArray, returnsArray, 'results-table');
 }
-form.addEventListener('submit', renderProgression)
-//calculate-results.addEventListener('submit', renderProgression)
+
+
+function isObjectEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+function resetCharts() {
+  if (
+    !isObjectEmpty(doughnutChartReference) &&
+    !isObjectEmpty(progressionChartReference)
+  ) {
+    doughnutChartReference.destroy();
+    progressionChartReference.destroy();
+  }
+}
+
+function clearForm() {
+  form['starting-amount'].value = '';
+  form['additional-contribution'].value = '';
+  form['time-amount'].value = '';
+  form['return-rate'].value = '';
+  form['tax-rate'].value = '';
+
+  resetCharts();
+
+  const errorInputContainers = document.querySelectorAll('.error');
+
+  for (const errorInputContainer of errorInputContainers) {
+    errorInputContainer.classList.remove('error');
+    errorInputContainer.parentElement.querySelector('p').remove();
+  }
+}
+function validateInput(evt) {
+  if (evt.target.value === '') {
+    return;
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+  const inputValue = evt.target.value.replace(',', '.');
+
+  if (
+    !parentElement.classList.contains('error') &&
+    (isNaN(inputValue) || Number(inputValue) <= 0)
+  ) {
+    // objetivo: <p class="text-red-500">Insira um valor numérico e maior que zero</p>
+    const errorTextElement = document.createElement('p'); //<p></p>
+    errorTextElement.classList.add('text-red-500'); //<p class='text-red-500'></p>
+    errorTextElement.innerText = 'Insira um valor numérico e maior que zero'; //<p class="text-red-500">Insira um valor numérico e maior que zero</p>
+
+    parentElement.classList.add('error');
+    grandParentElement.appendChild(errorTextElement);
+  } else if (
+    parentElement.classList.contains('error') &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove('error');
+    grandParentElement.querySelector('p').remove();
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === 'INPUT' && formElement.hasAttribute('name')) {
+    formElement.addEventListener('blur', validateInput);
+  }
+}
+
+const mainEl = document.querySelector('main');
+const carouselEl = document.getElementById('carousel');
+const nextButton = document.getElementById('slide-arrow-next');
+const previousButton = document.getElementById('slide-arrow-previous');
+
+nextButton.addEventListener('click', () => {
+  carouselEl.scrollLeft += mainEl.clientWidth;
+});
+previousButton.addEventListener('click', () => {
+  carouselEl.scrollLeft -= mainEl.clientWidth;
+});
+
+form.addEventListener('submit', renderProgression);
+// calculateButton.addEventListener('click', renderProgression);
+clearFormButton.addEventListener('click', clearForm);
--- src/investmentGoals.js
+++ src/investmentGoals.js
@@ -1,53 +1,58 @@
-function convertToMonthlyReturnRate(yearlyReturnRate){
-  return yearlyReturnRate **(1/12)
+function convertToMontlyReturnRate(yearlyReturnRate) {
+  return yearlyReturnRate ** (1 / 12);
 }
 
 export function generateReturnsArray(
-  startingAmount =0, 
-  timeHorizon =0, 
-  timePeriod = 'monthly', 
-  mounthlyContribution = 0, 
-  returnRate = 0, 
-  returnTimeFrame = 'monthly'){
+  startingAmount = 0,
+  timeHorizon = 0,
+  timePeriod = "monthly",
+  monthlyContribution = 0,
+  returnRate = 0,
+  returnTimeFrame = "monthly"
+) {
+  if (!timeHorizon || !startingAmount) {
+    throw new Error(
+      "Investimento inicial e prazo devem ser preenchidos com valores positivos."
+    );
+  }
 
-    if(!timeHorizon || !startingAmount){
-      throw new Error('Investimento inicial e prazo devem ser preenchidos com valores positivos.')
-    }
+  const finalReturnRate =
+    returnTimeFrame === "monthly"
+      ? 1 + returnRate / 100
+      : convertToMontlyReturnRate(1 + returnRate / 100);
 
-    const finalReturnRate = returnTimeFrame === 'monthly' 
-      ?  1 + returnRate/100
-      : convertToMonthlyReturnRate(1 + returnRate/100)
+  const finalTimeHorizon =
+    timePeriod === "monthly" ? timeHorizon : timeHorizon * 12;
 
-    const finalTimeHerizon = 
-      timePeriod === 'monthly' ? timeHorizon : timeHorizon * 12
+  const referenceInvestmentObject = {
+    investedAmount: startingAmount,
+    interestReturns: 0,
+    totalInterestReturns: 0,
+    month: 0,
+    totalAmount: startingAmount,
+  };
 
-    const referenceInvestimetObject = {
-      investedAmount : startingAmount,
-      interestReturns: 0,
-      totalInterestReturns: 0,
-      month: 0,
-      totalAmount: startingAmount,
-    }
+  const returnsArray = [referenceInvestmentObject];
+  for (
+    let timeReference = 1;
+    timeReference <= finalTimeHorizon;
+    timeReference++
+  ) {
+    const totalAmount =
+      returnsArray[timeReference - 1].totalAmount * finalReturnRate +
+      monthlyContribution;
+    const interestReturns =
+      returnsArray[timeReference - 1].totalAmount * (finalReturnRate - 1);
+    const investedAmount = startingAmount + monthlyContribution * timeReference;
+    const totalInterestReturns = totalAmount - investedAmount;
+    returnsArray.push({
+      investedAmount,
+      interestReturns,
+      totalInterestReturns,
+      month: timeReference,
+      totalAmount,
+    });
+  }
 
-    const returnsArray = [referenceInvestimetObject]
-    for (let timeReference = 1; timeReference <= finalTimeHerizon; timeReference++){
-      const totalAmount = 
-        returnsArray[timeReference - 1].totalAmount * finalReturnRate + mounthlyContribution
-      
-      const interestReturns = returnsArray[timeReference - 1].totalAmount * finalReturnRate
-
-      const investedAmount = startingAmount + mounthlyContribution * timeReference
-
-      const totalInterestReturns = totalAmount - investedAmount
-
-      returnsArray.push({
-        investedAmount,
-        interestReturns,
-        totalInterestReturns,
-        month: timeReference,
-        totalAmount,
-      })
-    }
-
-    return returnsArray
+  return returnsArray;
 }
